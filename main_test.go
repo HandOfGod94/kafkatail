1e9f62ae64361abf17b0088568148da77032aa41
Gahan Rakholia <gahan94rakh@gmail.com>
2021-06-22 23:11:17 +0530

    test: add tests for success case

--- main_test.go
+++ main_test.go
@@ -1,27 +1,36 @@
 package main_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/rendon/testcli"
 	"github.com/stretchr/testify/assert"
 )
 
+const appName = "kafkatail"
+
 func TestRequiredArgs(t *testing.T) {
 	testCases := []struct {
-		cmd     string
+		cliArgs string
 		want    string
 		wantErr bool
 	}{
-		{"kafkatail", `"bootstrap_servers, topic" not set`, true},
+		{"", `"bootstrap_servers, topic" not set`, true},
+		{"--bootstrap_servers=1.1.1.1:9093 --topic=test", "Hello World", false},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.want, func(t *testing.T) {
-			testcli.Run(tc.cmd)
+			testcli.Run(appName, strings.Split(tc.cliArgs, " ")...)
 
-			assert.Equal(t, tc.wantErr, testcli.Failure())
-			assert.Contains(t, testcli.Stderr(), tc.want)
+			if tc.wantErr {
+				assert.True(t, testcli.Failure())
+				assert.Contains(t, testcli.Stderr(), tc.want)
+			} else {
+				assert.True(t, testcli.Success())
+				assert.Contains(t, testcli.Stdout(), tc.want)
+			}
 		})
 	}
 }
