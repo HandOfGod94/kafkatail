2ef3c98c488becfb148ebc62c7b4c89f6fb6f02d
HandOfGod94 <gahan94rakh@gmail.com>
2021-07-02 16:19:56 +0530

    test: use table driven tests for consumer

--- consumer/kafka_consumer_test.go
+++ consumer/kafka_consumer_test.go
@@ -10,81 +10,80 @@ import (
 	"github.com/handofgod94/kafkatail/wire"
 	"github.com/segmentio/kafka-go"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/suite"
 )
 
 const defaultTimeout = 5 * time.Second
 
-type KafkaConsumerTestSuite struct {
-	suite.Suite
-}
+func TestConsumeSuccses(t *testing.T) {
+	type fields struct {
+		bootstrapServers []string
+		topic            string
+	}
 
-func TestKafkaConsumerSuite(t *testing.T) {
-	suite.Run(t, new(KafkaConsumerTestSuite))
-}
+	testCases := []struct {
+		desc      string
+		fields    fields
+		opts      consumer.Options
+		msgToSend []string
+		want      string
+	}{
+		{
+			desc: "with default values",
+			fields: fields{
+				bootstrapServers: []string{"localhost:9093"},
+				topic:            "kafkatail-test-topic",
+			},
+			msgToSend: []string{"hello", "world"},
+			want:      "hello",
+		},
+		{
+			desc: "with partition option",
+			fields: fields{
+				bootstrapServers: []string{"localhost:9093"},
+				topic:            "kafkatail-test-topic",
+			},
+			msgToSend: []string{"hello", "world"},
+			opts:      consumer.Options{Partition: 0},
+			want:      "hello",
+		},
+		{
+			desc: "with offsett option",
+			fields: fields{
+				bootstrapServers: []string{"localhost:9093"},
+				topic:            "kafkatail-test-topic",
+			},
+			msgToSend: []string{"hello", "world"},
+			opts:      consumer.Options{Offset: kafka.LastOffset},
+			want:      "hello",
+		},
+	}
 
-func (kct *KafkaConsumerTestSuite) SetupSuite() {
-	createTopicWithConfig(kct.T(), context.Background(), kafka.TopicConfig{
+	createTopicWithConfig(t, context.Background(), kafka.TopicConfig{
 		Topic:             "kafkatail-test-topic",
 		NumPartitions:     2,
 		ReplicationFactor: 1,
 	})
-}
+	defer deleteTopic(t, context.Background(), "kafkatail-test-topic")
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
 
-func (kct *KafkaConsumerTestSuite) TearDownSuite() {
-	deleteTopic(kct.T(), context.Background(), "kafkatail-test-topic")
-}
+			w := bytes.NewBufferString("")
+			ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+			defer cancel()
 
-func (s *KafkaConsumerTestSuite) TestConsume_Success() {
-	w := bytes.NewBufferString("")
-	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
-	defer cancel()
+			c := consumer.New(tc.fields.bootstrapServers, tc.fields.topic)
+			go func(ctx context.Context) {
+				c.Consume(ctx, w, wire.NewPlaintextDecoder())
+			}(ctx)
 
-	c := consumer.New([]string{"localhost:9093"}, "kafkatail-test-topic")
-	go func(ctx context.Context) {
-		c.Consume(ctx, w, wire.NewPlaintextDecoder())
-	}(ctx)
+			sendMessage(t, ctx, tc.fields.bootstrapServers, tc.fields.topic, nil, []byte("hello"))
+			sendMessage(t, ctx, tc.fields.bootstrapServers, tc.fields.topic, nil, []byte("world"))
 
-	sendMessage(s.T(), ctx, []string{"localhost:9093"}, "kafkatail-test-topic", []byte("foo"), []byte("hello"))
-	sendMessage(s.T(), ctx, []string{"localhost:9093"}, "kafkatail-test-topic", []byte("foo"), []byte("world"))
-
-	<-ctx.Done()
-	got := w.String()
-	assert.Contains(s.T(), got, "hello\n")
-	assert.Contains(s.T(), got, "world\n")
-}
-
-func (s *KafkaConsumerTestSuite) TestConsume_WithParition() {
-	w := bytes.NewBufferString("")
-	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
-	defer cancel()
-
-	c := consumer.Options{Partition: 0}.New([]string{"localhost:9093"}, "kafkatail-test-topic")
-	go func(ctx context.Context) {
-		c.Consume(ctx, w, wire.NewPlaintextDecoder())
-	}(ctx)
-
-	sendMessage(s.T(), ctx, []string{"localhost:9093"}, "kafkatail-test-topic", []byte("foo"), []byte("hello"))
-	sendMessage(s.T(), ctx, []string{"localhost:9093"}, "kafkatail-test-topic", []byte("bar"), []byte("world"))
-
-	<-ctx.Done()
-	assert.Contains(s.T(), w.String(), "Partition: 0")
-}
-
-func (s *KafkaConsumerTestSuite) TestConsumer_WithOffeset() {
-	w := bytes.NewBufferString("")
-	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
-	defer cancel()
-
-	sendMessage(s.T(), ctx, []string{"localhost:9093"}, "kafkatail-test-topic", []byte("foo"), []byte("hello"))
-	sendMessage(s.T(), ctx, []string{"localhost:9093"}, "kafkatail-test-topic", []byte("foo"), []byte("world"))
-
-	c := consumer.Options{Offset: kafka.FirstOffset}.New([]string{"localhost:9093"}, "kafkatail-test-topic")
-	c.Consume(ctx, w, wire.NewPlaintextDecoder())
-
-	got := w.String()
-	assert.Contains(s.T(), got, "hello\n")
-	assert.Contains(s.T(), got, "hello\n")
+			<-ctx.Done()
+			got := w.String()
+			assert.Contains(t, got, tc.want)
+		})
+	}
 }
 
 func TestCreateConsumerWithOptions(t *testing.T) {
