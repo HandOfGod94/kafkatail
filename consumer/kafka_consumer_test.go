e6d6ab9c81c322911e6680350d1ec97643a08ebd
HandOfGod94 <gahan94rakh@gmail.com>
2021-07-26 01:40:50 +0530

    fix: honor group_id option while initializing reader (#2)

--- consumer/kafka_consumer_test.go
+++ consumer/kafka_consumer_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"strings"
 	"testing"
 	"time"
 
@@ -14,7 +15,7 @@ import (
 	"gopkg.in/tomb.v2"
 )
 
-const defaultTimeout = 5 * time.Second
+const defaultTimeout = 10 * time.Second
 
 func TestConsumeSuccses(t *testing.T) {
 	type fields struct {
@@ -124,6 +125,78 @@ func TestConsume_Errors(t *testing.T) {
 
 			<-tb.Dead()
 			assert.Contains(t, tb.Err().Error(), tc.expectedErr)
+		})
+	}
+}
+
+func TestConsumeWithMultipleParitions(t *testing.T) {
+	type partition = int
+	type wantFields struct {
+		parition string
+		offset   string
+		message  string
+	}
+	topic := "kafka-consume-gorup-id-test"
+
+	testCases := []struct {
+		desc             string
+		opts             consumer.Options
+		bootstrapServers []string
+		topic            string
+		messages         map[partition]string
+		want             []wantFields
+	}{
+		{
+			desc: "with group id options",
+			opts: consumer.Options{GroupID: "foo-test-id"},
+			messages: map[partition]string{
+				0: "foo",
+				1: "bar",
+			},
+			bootstrapServers: []string{"localhost:9093"},
+			topic:            topic,
+			want: []wantFields{
+				{"Partition: 0", "Offset: 0", "foo"},
+				{"Partition: 1", "Offset: 0", "bar"},
+			},
+		},
+	}
+
+	createTopicWithConfig(t, context.Background(), kafka.TopicConfig{
+		Topic:             topic,
+		NumPartitions:     2,
+		ReplicationFactor: 1,
+	})
+	defer deleteTopic(t, context.Background(), topic)
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+			defer cancel()
+
+			tb, tctx := tomb.WithContext(ctx)
+			c := tc.opts.New(tc.bootstrapServers, tc.topic)
+			outChan := c.Consume(tctx, tb, wire.NewPlaintextDecoder())
+
+			for partition, msg := range tc.messages {
+				sendMessageToPartition(t, ctx, tc.bootstrapServers, tc.topic, partition, nil, []byte(msg))
+			}
+
+			var got strings.Builder
+			loop := true
+			for loop {
+				select {
+				case msg := <-outChan:
+					got.WriteString(msg)
+				case <-tb.Dead():
+					loop = false
+				}
+			}
+
+			for _, wt := range tc.want {
+				assert.Contains(t, got.String(), wt.parition)
+				assert.Contains(t, got.String(), wt.offset)
+				assert.Contains(t, got.String(), wt.message)
+			}
 		})
 	}
 }
