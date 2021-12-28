544ef31d721a2909a1f467f8281e9cab461578f2
HandOfGod94 <gahan94rakh@gmail.com>
2021-12-28 22:34:07 +0530

    refactor: move formatIDs to decoder

--- wire/decoder.go
+++ wire/decoder.go
@@ -1,5 +1,19 @@
 package wire
 
+import "github.com/thediveo/enumflag"
+
+type Format enumflag.Flag
+
+const (
+	PlainText Format = iota
+	Proto
+)
+
+var FormatIDs = map[Format][]string{
+	PlainText: {"plaintext"},
+	Proto:     {"proto"},
+}
+
 type Decoder interface {
 	Decode([]byte) (string, error)
 }
