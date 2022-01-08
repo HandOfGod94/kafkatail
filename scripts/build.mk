9e1c9ff49db8ef5e41dd52eb0be42c5d1e2ab8dd
HandOfGod94 <gahan94rakh@gmail.com>
2022-01-08 16:01:43 +0530

    remove old integration-tests
    Because:
    - we now have cucumber feature tests
    
    additional changes:
    - move makefile targets to script
    - update readme

---
+++ scripts/build.mk
@@ -0,0 +1,40 @@
+APP_NAME=kafkatail
+
+all: clean fmt tidy build install
+
+clean:
+	rm -rf out/
+
+install:
+	go install
+
+quality-check:
+	staticcheck ./...
+	gocyclo -over 15 -ignore "testdata/" .
+	gocognit -over 15 .
+
+clean-integration:
+	docker compose stop
+	docker compose rm -f -v
+	docker volume rm -f kafkatail_kafka_data kafkatail_zookeeper_data
+	docker compose up -d
+
+test:
+	go test -timeout 60s -cover -count=1 -coverprofile=coverage.out ./...
+	go tool cover -func=coverage.out
+
+tidy:
+	go mod tidy -v
+
+run: dev
+	./out/$(APP_NAME)
+
+dev:
+	go build -v -o out/$(APP_NAME) main.go
+
+build:
+	go build -v -ldflags "-w" -o out/$(APP_NAME) main.go
+
+fmt:
+	go fmt ./...
+	gofmt -s -w .
