8f9b0a3dd083f71a24aed42c9e4d40704f76f758
HandOfGod94 <gahan94rakh@gmail.com>
2022-06-12 13:22:36 +0530

    feat: introduce new multiple partition consumer

---
+++ consumer/multiple_partition_consumer.go
@@ -0,0 +1,72 @@
+package consumer
+
+import (
+	"context"
+	"log"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/segmentio/kafka-go"
+)
+
+var _ ClosableConsumer = (*MultiplePartitionConsumer)(nil)
+
+type MultiplePartitionConsumer struct {
+	readers []*kafka.Reader
+}
+
+type MultiplePartitionConsumerOpts struct {
+	BootstrapServers []string `validate:"required"`
+	Topic            string   `validate:"required"`
+	Offset           int64
+}
+
+func NewMultiplePartitionConsumer(ctx context.Context, opts MultiplePartitionConsumerOpts) (*MultiplePartitionConsumer, error) {
+	log.Printf("starting multiple partition consumer with config: %+v", opts)
+	validate := validator.New()
+	if errs := validate.Struct(opts); errs != nil {
+		return nil, errs
+	}
+
+	partitions, err := opts.Paritions(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	readers := make([]*kafka.Reader, len(partitions))
+	for i, partition := range partitions {
+		readers[i] = kafka.NewReader(kafka.ReaderConfig{
+			Brokers:   opts.BootstrapServers,
+			Topic:     opts.Topic,
+			Partition: partition.ID,
+		})
+	}
+
+	return &MultiplePartitionConsumer{readers}, nil
+}
+
+func (opts *MultiplePartitionConsumerOpts) Paritions(ctx context.Context) ([]kafka.Partition, error) {
+	conn, err := kafka.DialContext(ctx, "tcp", opts.BootstrapServers[0])
+	if err != nil {
+		return nil, err
+	}
+
+	return conn.ReadPartitions(opts.Topic)
+}
+
+func (mpc *MultiplePartitionConsumer) Consume(ctx context.Context, decoder WireDecoder) <-chan Result {
+	out := make(chan Result)
+
+	for _, reader := range mpc.readers {
+		go func(partitionOut <-chan Result) {
+			for res := range partitionOut {
+				out <- res
+			}
+		}(printMsgs(ctx, reader, decoder))
+	}
+
+	return out
+}
+
+func (mpc *MultiplePartitionConsumer) Close() error {
+	return nil
+}
---
+++ consumer/multiple_partition_consumer_test.go
@@ -0,0 +1,92 @@
+package consumer_test
+
+import (
+	"context"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/handofgod94/kafkatail/consumer"
+	"github.com/handofgod94/kafkatail/kafkatest"
+	"github.com/handofgod94/kafkatail/wire"
+	"github.com/segmentio/kafka-go"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type MultipleParitionTestSuite struct {
+	suite.Suite
+	opts consumer.MultiplePartitionConsumerOpts
+}
+
+func (suite *MultipleParitionTestSuite) SetupSuite() {
+	suite.opts = consumer.MultiplePartitionConsumerOpts{
+		BootstrapServers: []string{"localhost:9093"},
+		Topic:            "kafkatail-test-topic",
+		Offset:           kafka.FirstOffset,
+	}
+
+	kafkatest.CreateTopicWithConfig(context.Background(), kafka.TopicConfig{
+		Topic:             suite.opts.Topic,
+		NumPartitions:     2,
+		ReplicationFactor: 1,
+	})
+}
+
+func (suite *MultipleParitionTestSuite) TearDownSuite() {
+	kafkatest.DeleteTopic(context.Background(), suite.opts.Topic)
+}
+
+func (suite *MultipleParitionTestSuite) TestListPartitions_FetchesAllAvailablePartitions() {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	partitions, err := suite.opts.Paritions(ctx)
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), 2, len(partitions))
+}
+
+func (suite *MultipleParitionTestSuite) TestListPartitions_RetrunsErrorForInvalidConfig() {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	opts := consumer.MultiplePartitionConsumerOpts{
+		BootstrapServers: []string{"foobar:9093"},
+		Topic:            "kafkatail-test-topic",
+		Offset:           kafka.FirstOffset,
+	}
+
+	partitions, err := opts.Paritions(ctx)
+	assert.Error(suite.T(), err)
+	assert.Empty(suite.T(), partitions)
+}
+
+func (suite *MultipleParitionTestSuite) TestConsume() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	gc, _ := consumer.NewMultiplePartitionConsumer(ctx, suite.opts)
+	defer gc.Close()
+
+	resultChan := gc.Consume(ctx, wire.NewPlaintextDecoder())
+
+	kafkatest.SendMultipleMessagesToPartition(suite.T(), suite.opts.BootstrapServers, suite.opts.Topic, map[partition]string{
+		0: "foo",
+		1: "bar",
+	})
+
+	results := kafkatest.ReadChanMessages(ctx, resultChan)
+	var got strings.Builder
+	for _, res := range results[:len(results)-1] {
+		got.WriteString(res.Message)
+	}
+
+	assert.Contains(suite.T(), got.String(), "Partition: 0")
+	assert.Contains(suite.T(), got.String(), "Partition: 1")
+	assert.Contains(suite.T(), got.String(), "foo")
+	assert.Contains(suite.T(), got.String(), "bar")
+}
+
+func TestListPartitionSuite(t *testing.T) {
+	suite.Run(t, new(MultipleParitionTestSuite))
+}
