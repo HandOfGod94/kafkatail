24dcc1421a95c8a1793434c768e9d5b58a5b3305
Gahan Rakholia <gahan94rakh@gmail.com>
2022-11-03 15:54:14 +0530

    support printing of headers while tailing

--- consumer/printer.go
+++ consumer/printer.go
@@ -24,15 +24,21 @@ func printMsgs(ctx context.Context, reader *kafka.Reader, decoder WireDecoder) <
 				return
 			}
 
+			msg := bytes.NewBufferString("")
+			fmt.Fprintln(msg, "====================Message====================")
+			fmt.Fprintf(msg, "============Partition: %v, Offset: %v==========\n", m.Partition, m.Offset)
+			fmt.Fprintln(msg, "====================Header====================")
+			for _, header := range m.Headers {
+				fmt.Fprintf(msg, "%s: %s\n", header.Key, string(header.Value))
+			}
+
 			value, err := decoder.Decode(m.Value)
 			if err != nil {
 				log.Printf("failed to decode message. error: %v", err)
 				resultChan <- Result{Err: err}
 				return
 			}
-			msg := bytes.NewBufferString("")
-			fmt.Fprintln(msg, "====================Message====================")
-			fmt.Fprintf(msg, "============Partition: %v, Offset: %v==========\n", m.Partition, m.Offset)
+			fmt.Fprintln(msg, "====================Payload====================")
 			fmt.Fprintln(msg, value)
 			resultChan <- Result{Message: msg.String()}
 		}
